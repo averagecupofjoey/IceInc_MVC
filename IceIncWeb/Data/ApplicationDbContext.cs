8a85f320a47dbdb9398227183727e5583ff36588
Joseph Elias <EliasJo@ddc.nyc.gov>
2023-11-09 16:37:56 -0500

    Connected entity framework and SQL database

---
+++ IceIncWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using IceIncWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IceIncWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //creates table
+        public DbSet<Category> Categories { get; set; }
+    }
+}
